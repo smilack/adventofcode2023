1597ceee7105f7fb390e1823221de06ef282df24
Tom Smilack <tom@smilack.net>
2023-12-02 16:28:56 -0600

    Create day 2

---
+++ src/Two.purs
@@ -0,0 +1,25 @@
+module AdventOfCode.Twenty23.Two
+  ( main
+  ) where
+
+import Prelude
+import AdventOfCode.Twenty23.Util
+import Data.String (split)
+import Data.String.Pattern (Pattern(..))
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Console (log, logShow)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readTextFile)
+
+main :: Effect Unit
+main = launchAff_ do
+  input <- readTextFile UTF8 "./input/2"
+  liftEffect do
+    log "Part 1:"
+    -- log ""
+    -- logShow $ solve1 input
+    log "Part2:"
+    -- log ""
+    -- logShow $ solve2 input
