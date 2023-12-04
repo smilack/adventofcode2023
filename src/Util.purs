f8fad44833631a2a8474de66cb39d10523a5a4a5
Tom Smilack <tom@smilack.net>
2023-12-04 10:00:40 -0600

    Utility function to extract number from array of whatevers and sum them

--- src/Util.purs
+++ src/Util.purs
@@ -1,5 +1,6 @@
 module AdventOfCode.Twenty23.Util
   ( lines
+  , sumMap
   , testParser
   , to2dArray
   ) where
@@ -7,13 +8,18 @@ module AdventOfCode.Twenty23.Util
 import Prelude
 
 import Control.Monad.Error.Class (class MonadThrow)
+import Data.Array (foldl)
 import Data.Either (Either(..))
 import Data.String (split)
 import Data.String.CodeUnits (toCharArray)
 import Data.String.Pattern (Pattern(..))
 import Effect.Aff (Error)
 import Parsing (Parser, runParser)
+import PointFree ((<..))
 import Test.Spec.Assertions (shouldEqual)
+
+sumMap :: forall a. (a -> Int) -> Array a -> Int
+sumMap = foldl add 0 <.. map
 
 to2dArray :: String -> Array (Array Char)
 to2dArray = map toCharArray <<< lines
