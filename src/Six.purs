79c3fe2220c1278eefa8dd305b5ebad87d74e80e
Tom Smilack <tom@smilack.net>
2023-12-06 20:02:33 -0600

    D6P1 solution

--- src/Six.purs
+++ src/Six.purs
@@ -1,25 +1,71 @@
 module AdventOfCode.Twenty23.Six
-  ( main
+  ( NEAR
+  , Race
+  , findWins
+  , main
+  , numWins
+  , parseRaces
+  , score
+  , solve1
   ) where
 
-import Prelude
 import AdventOfCode.Twenty23.Util
+import Prelude
 
+import Control.Monad.List.Trans (foldl)
+import Data.Array (filter, length, range)
+import Data.Array.NonEmpty (NonEmptyArray, foldl1, zipWith)
+import Data.Either (Either)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
-import Parsing (Parser)
+import Parsing (ParseError(..), Parser, runParser)
+import Parsing.Combinators.Array (many1)
+import Parsing.String.Basic (intDecimal, skipSpaces)
 
 main :: Effect Unit
 main = launchAff_ do
   input <- readTextFile UTF8 "./input/6"
   liftEffect do
     log "Part 1:"
-    -- log ""
-    -- logShow $ solve1 input
+    log "Number of different ways to win"
+    logShow $ solve1 input
     log "Part2:"
-    -- log ""
-    -- logShow $ solve2 input
+
+-- log ""
+-- logShow $ solve2 input
+
+type Race = { time :: Int, dist :: Int }
+
+type NEAR = NonEmptyArray Race
+
+parseRaces :: Parser String NEAR
+parseRaces = do
+  skip "Time:"
+  times <- intList
+  skipSpaces
+  skip "Distance:"
+  distances <- intList
+  pure $ zipWith { time: _, dist: _ } times distances
+  where
+  intList = many1 do
+    skipSpaces
+    intDecimal
+
+score :: Int -> Int -> Int
+score time hold = hold * (time - hold)
+
+findWins :: Race -> Array Int
+findWins { time, dist } =
+  filter ((_ > dist) <<< score time) (range 1 $ time - 1)
+
+numWins :: Race -> Int
+numWins = length <<< findWins
+
+solve1 :: String -> Either ParseError Int
+solve1 input = do
+  races <- runParser input parseRaces
+  pure $ foldl1 (*) $ map numWins races
--- test/Six.purs
+++ test/Six.purs
@@ -2,9 +2,12 @@ module Test.AdventOfCode.Twenty23.Six
   ( main
   ) where
 
-import Prelude
 import AdventOfCode.Twenty23.Six
 import AdventOfCode.Twenty23.Util
+import Prelude
+
+import Data.Array.NonEmpty (cons')
+import Data.Either (Either(..))
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
@@ -20,7 +23,27 @@ main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
   describe "Day Six" do
     describe "Part 1" do
-      pending "parse input"
-      pending "other stuff"
+      it "parses input" do
+        testParser input parsed parseRaces
+      it "findsWins" do
+        findWins
+          { time: 7, dist: 9 }
+          `shouldEqual`
+            [ 2, 3, 4, 5 ]
+      it "numWins" do
+        numWins { time: 15, dist: 40 } `shouldEqual` 8
+        numWins { time: 30, dist: 200 } `shouldEqual` 9
+      it "solve1" do
+        solve1 input `shouldEqual` Right 288
     describe "Part 2" do
       pending "more stuff"
+
+input :: String
+input = "Time:      7  15   30\nDistance:  9  40  200"
+
+parsed :: NEAR
+parsed = cons'
+  { time: 7, dist: 9 }
+  [ { time: 15, dist: 40 }
+  , { time: 30, dist: 200 }
+  ]
