b672090523217ca45fb098ed232cc65f9ee85858
Tom Smilack <tom@smilack.net>
2023-12-08 23:22:04 -0600

    Create day 9

---
+++ test/Nine.purs
@@ -0,0 +1,26 @@
+module Test.AdventOfCode.Twenty23.Nine
+  ( main
+  ) where
+
+import Prelude
+import AdventOfCode.Twenty23.Nine
+import AdventOfCode.Twenty23.Util
+import Data.String (split)
+import Data.String.Pattern (Pattern(..))
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Test.QuickCheck ((===), Result)
+import Test.Spec (Spec, pending, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.QuickCheck (quickCheck)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
+
+main :: Effect Unit
+main = launchAff_ $ runSpec [ consoleReporter ] do
+  describe "Day Nine" do
+    describe "Part 1" do
+      pending "parse input"
+      pending "other stuff"
+    describe "Part 2" do
+      pending "more stuff"
