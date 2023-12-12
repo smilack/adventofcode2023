522bcb539690eb7a8aeb59cd2728079869356166
Tom Smilack <tom@smilack.net>
2023-12-12 00:16:55 -0600

    Day 11 part 1 solution

--- src/Eleven.purs
+++ src/Eleven.purs
@@ -1,25 +1,143 @@
 module AdventOfCode.Twenty23.Eleven
-  ( main
+  ( Coord
+  , Image(..)
+  , Space(..)
+  , _cell
+  , _col
+  , _row
+  , expand
+  , findEmpty
+  , findGalaxies
+  , getShortestPaths
+  , isEmpty
+  , main
+  , parseImage
+  , solve1
   ) where
 
-import Prelude
 import AdventOfCode.Twenty23.Util
+import Prelude
 
+import Control.Alt ((<|>))
+import Data.Array (foldMap, foldr, fromFoldable, insertAt, intercalate, snoc, sortBy, (!!))
+import Data.Either (Either)
+import Data.Foldable (sum)
+import Data.Lens (class Wander, Indexed, IndexedOptic', IndexedTraversal, IndexedTraversal', Iso', Traversal', allOf, filtered, iso, itoListOf, lengthOf, preview, traversed)
+import Data.Lens.Index (class Index, ix)
+import Data.Lens.Indexed (itraversed)
+import Data.Lens.Types (AffineTraversal')
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.Ord (abs)
+import Data.Ordering (invert)
+import Data.TraversableWithIndex (class TraversableWithIndex)
+import Data.Tuple (Tuple(..), fst)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
-import Parsing (Parser)
+import Parsing (ParseError(..), Parser, runParser)
+import Parsing.Combinators (sepEndBy, (<?>))
+import Parsing.Combinators.Array (many)
+import Parsing.String (char)
 
 main :: Effect Unit
 main = launchAff_ do
   input <- readTextFile UTF8 "./input/11"
   liftEffect do
     log "Part 1:"
-    -- log ""
-    -- logShow $ solve1 input
+    log "Sum of shortest paths between galaxies"
+    logShow $ solve1 input
     log "Part2:"
-    -- log ""
-    -- logShow $ solve2 input
+
+-- log ""
+-- logShow $ solve2 input
+
+solve1 :: String -> Either ParseError Int
+solve1 input =
+  getShortestPaths
+    <$> findGalaxies
+    <$> expand
+    <$> runParser input parseImage
+
+data Space = Galaxy | Empty
+
+derive instance Eq Space
+
+instance Show Space where
+  show Galaxy = "#"
+  show Empty = "."
+
+newtype Image = Image (Array (Array Space))
+
+derive instance Newtype Image _
+
+instance Show Image where
+  show (Image i) = intercalate "\n" $ foldMap show <$> i
+
+parseImage :: Parser String Image
+parseImage = wrap <$> fromFoldable <$> parseLine `sepEndBy` char '\n'
+  where
+  parseLine = many do
+    Empty <$ char '.'
+      <|> Galaxy <$ char '#'
+        <?> "Space (# or .)"
+
+isEmpty :: Traversal' Image Space -> Image -> Boolean
+isEmpty t i =
+  lengthOf t i > 0
+    && allOf t (_ == Empty) i
+
+_image :: Iso' Image (Array (Array Space))
+_image = iso unwrap wrap
+
+_col :: Int -> Traversal' Image Space
+_col i = _image <<< traversed <<< ix i
+
+_row :: Int -> Traversal' Image Space
+_row i = _image <<< ix i <<< traversed
+
+_cell :: Coord -> AffineTraversal' Image Space
+_cell { x, y } = _image <<< ix y <<< ix x
+
+isGalaxy :: Coord -> Image -> Boolean
+isGalaxy c i = preview (_cell c) i == Just Galaxy
+
+findEmpty :: (Int -> Traversal' Image Space) -> Image -> Array Int
+findEmpty t i = go 0 []
+  where
+  go x found
+    | lengthOf (t x) i <= 0 = found
+    | allOf (t x) (_ == Empty) i = go (inc x) (snoc found x)
+    | otherwise = go (inc x) found
+
+expand :: Image -> Image
+expand i@(Image arr) = Image $
+  foldr copy (foldr (map <<< copy) arr cols) rows
+  where
+  rows = findEmpty _row i
+  cols = findEmpty _col i
+
+copy :: forall a. Int -> Array a -> Array a
+copy x a = fromMaybe a do
+  r <- a !! x
+  insertAt x r a
+
+type Coord = { x :: Int, y :: Int }
+
+findGalaxies :: Image -> Array Coord
+findGalaxies i = go { x: 0, y: 0 } []
+  where
+  go c@{ x, y } found
+    | lengthOf (_row y) i == 0 = found
+    | x >= lengthOf (_row y) i = go { x: 0, y: inc y } found
+    | isGalaxy c i = go { x: inc x, y } (snoc found c)
+    | otherwise = go { x: inc x, y } found
+
+getShortestPaths :: Array Coord -> Int
+getShortestPaths arr = (_ / 2) $ sum do
+  c1 <- arr
+  c2 <- arr
+  pure $ abs (c1.x - c2.x) + abs (c1.y - c2.y)
--- test/Eleven.purs
+++ test/Eleven.purs
@@ -1,17 +1,21 @@
 module Test.AdventOfCode.Twenty23.Eleven
-  ( main
+  ( example
+  , main
   ) where
 
-import Prelude
 import AdventOfCode.Twenty23.Eleven
 import AdventOfCode.Twenty23.Util
+import Prelude
+
+import Data.Either (Either(..))
 import Data.String (split)
 import Data.String.Pattern (Pattern(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Parsing (runParser)
 import Test.QuickCheck ((===), Result)
 import Test.Spec (Spec, pending, describe, it)
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions (fail, shouldEqual)
 import Test.Spec.QuickCheck (quickCheck)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
@@ -20,7 +24,90 @@ main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
   describe "Day Eleven" do
     describe "Part 1" do
-      pending "parse input"
-      pending "other stuff"
+      case runParser example parseImage of
+        Left _ ->
+          it "parse input" $ fail "parse error"
+        Right i -> do
+          it "parse input" $ show i `shouldEqual` example
+          describe "isEmpty" do
+            it "_col 0 false" $ isEmpty (_col 0) i `shouldEqual` false
+            it "_col 1 false" $ isEmpty (_col 1) i `shouldEqual` false
+            it "_col 2 true" $ isEmpty (_col 2) i `shouldEqual` true
+            it "_col 3 false" $ isEmpty (_col 3) i `shouldEqual` false
+            it "_row 0 false" $ isEmpty (_row 0) i `shouldEqual` false
+            it "_row 1 false" $ isEmpty (_row 1) i `shouldEqual` false
+            it "_row 2 false" $ isEmpty (_row 2) i `shouldEqual` false
+            it "_row 3 true" $ isEmpty (_row 3) i `shouldEqual` true
+          it "empty rows" do
+            findEmpty _row i `shouldEqual` [ 3, 7 ]
+          it "empty cols" do
+            findEmpty _col i `shouldEqual` [ 2, 5, 8 ]
+          let
+            ex = expand i
+          it "expand" do
+            show ex `shouldEqual` expanded
+          let
+            gals = findGalaxies ex
+          it "findGalaxies" do
+            gals `shouldEqual` galaxies
+          it "shortest paths" do
+            getShortestPaths gals `shouldEqual` 374
+      it "solve part1" do
+        solve1 example `shouldEqual` Right 374
     describe "Part 2" do
       pending "more stuff"
+
+example :: String
+example =
+  """...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."""
+
+expanded :: String
+expanded =
+  """....#........
+.........#...
+#............
+.............
+.............
+........#....
+.#...........
+............#
+.............
+.............
+.........#...
+#....#......."""
+
+galaxies :: Array Coord
+galaxies =
+  [ { x: 4, y: 0 }
+  , { x: 9, y: 1 }
+  , { x: 0, y: 2 }
+  , { x: 8, y: 5 }
+  , { x: 1, y: 6 }
+  , { x: 12, y: 7 }
+  , { x: 9, y: 10 }
+  , { x: 0, y: 11 }
+  , { x: 5, y: 11 }
+  ]
+
+--  0123456789ABC
+-- 0....1........
+-- 1.........2...
+-- 23............
+-- 3.............
+-- 4.............
+-- 5........4....
+-- 6.5...........
+-- 7............6
+-- 8.............
+-- 9.............
+-- A.........7...
+-- B8....9.......
